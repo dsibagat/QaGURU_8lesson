8abfc07cdb7a409717a710a91d30c72e80ef4cf9
dsibagat <Damir.Sibagatulin@t-systems.com>
2021-02-23 16:15:31 +0300

    added selenoid support

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,11 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+    @BeforeAll
+    static void setup() {
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud:4444/wd/hub/";
+    }
+}

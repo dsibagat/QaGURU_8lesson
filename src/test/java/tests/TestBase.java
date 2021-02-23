484caf90367e5aec297f0c926d1de9442a080dcd
dsibagat <Damir.Sibagatulin@t-systems.com>
2021-02-23 17:07:21 +0300

    add video and parameters

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,11 +1,52 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static helpers.AttachmentsHelper.*;
 
 public class TestBase {
     @BeforeAll
     static void setup() {
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud:4444/wd/hub/";
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.startMaximized = true;
+
+        if (System.getProperty("remote_driver") != null) {
+            // config for Java + Selenide
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("enableVNC", true);
+            capabilities.setCapability("enableVideo", true);
+            Configuration.browserCapabilities = capabilities;
+            Configuration.remote = System.getProperty("remote_driver");
+
+            // config for Java + Selenium
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("browserName", "chrome");
+//        capabilities.setCapability("browserVersion", "87.0");
+//        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+//                "enableVNC", true,
+//                "enableVideo", true
+//        ));
+//        RemoteWebDriver driver = new RemoteWebDriver(
+//                URI.create("http://selenoid:4444/wd/hub").toURL(),
+//                capabilities
+//        );
+        }
+    }
+
+    @AfterEach
+    public void afterEach() {
+        attachScreenshot("Last screenshot");
+        attachPageSource();
+        attachAsText("Browser console logs", getConsoleLogs());
+        if (System.getProperty("video_storage") != null)
+            attachVideo();
+        closeWebDriver();
     }
 }
